103db84f4abc526195a84b38c0ba96bf343e46d6
冯在耀 <fengzaiyao@delonix.group>
2023-07-26 12:55:25 +0800

    v5 多维动态规划

---
+++ src/main/java/io/github/fengzaiyao/leetcode/力扣热题100/v5/_16_多维动态规划/_01_不同路径.java
@@ -0,0 +1,27 @@
+package io.github.fengzaiyao.leetcode.力扣热题100.v5._16_多维动态规划;
+
+// https://leetcode.cn/problems/unique-paths/?envType=study-plan-v2&envId=top-100-liked
+
+public class _01_不同路径 {
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3, 7));
+    }
+
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
---
+++ src/main/java/io/github/fengzaiyao/leetcode/力扣热题100/v5/_16_多维动态规划/_04_最长公共子序列.java
@@ -0,0 +1,26 @@
+package io.github.fengzaiyao.leetcode.力扣热题100.v5._16_多维动态规划;
+
+// https://leetcode.cn/problems/longest-common-subsequence/?envType=study-plan-v2&id=top-100-liked
+
+public class _04_最长公共子序列 {
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonSubsequence("abcde", "ace"));
+    }
+
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
